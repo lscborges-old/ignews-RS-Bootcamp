424b3c4a938c9a8e52743eb27738e36eed30da21
Lucas Borges <lscborges1@gmail.com>
2021-04-07 12:20:39 -0300

    Added  _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import styles from '../styles/home.module.css'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello</h1>
+    <h1 className={styles.title}>Hello
+    <span> Memes</span>
+    
+    </h1>
+
   )
 }
